600ac03671fead8798733fe79de9704e84656ab0
oNk2r <onkartulshigar@gmail.com>
2026-07-09 02:20:04 +0530

    feat: add ResumeCard and ScoreCircle components for displaying resumes with feedback
    - Implemented ResumeCard component to display individual resume details including company name, job title, and overall score.
    - Created ScoreCircle component to visually represent the overall score of the resume using a circular progress indicator.
    - Updated home route to render a list of ResumeCard components based on the resumes data.
    - Removed unused welcome component and associated assets (logo images and welcome page).
    - Added constants for sample resumes and their feedback structure.
    - Defined TypeScript interfaces for Job, Resume, and Feedback to ensure type safety across the application.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumify" },
+    { name: "description", content: "Analyze your resume with AI" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center min-h-screen">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your resume and get AI-powered feedback</h2>
+      </div>
+    
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+            ))}
+        </div>
+      )}
+
+    </section>
+  </main>
 }
